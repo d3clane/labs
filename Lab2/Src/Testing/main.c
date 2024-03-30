df6541e7826aaa0f4a6487a8777d800bc58ac9c9
Arman <ovakimian.al@phystech.edu>
2024-03-30 15:24:15 +0300

    uncommented main

--- Lab2/Src/Testing/main.c
+++ Lab2/Src/Testing/main.c
@@ -1,13 +1,10 @@
 #include "Testing.h"
 #include "Sorts/Sorts.h"
 
-#include <stdio.h>
-
 int main()
 {
-    
-    /*
     //-----------------------Point 1-------------------------------------------
+
     TestSort("Tests/small_tests", "TestsResults/Point1/Bubble.out",    
                                             0, 1000, 50, 5, BubbleSort   );
     TestSort("Tests/small_tests", "TestsResults/Point1/Insertion.out", 
@@ -17,29 +14,20 @@ int main()
     TestSort("Tests/small_tests", "TestsResults/Point1/Shell.out", 
                                             0, 1000, 50, 5, ShellSort    );
 
-    
-    */
-
     //-----------------------Point 2-------------------------------------------
 
-    /*    
     TestHeapSort("Tests/big_tests", "TestsResults/Point2/HeapSortTestRank", 
                                             0, 1000000, 10000, 1, HeapSortTestRank);
-    */
 
     //-----------------------Point 3-------------------------------------------
-    
-    /*
     
     TestSort("Tests/big_tests", "TestsResults/Point3/RecursiveMerge.out", 
                                             0, 1000000, 10000, 1, RecursiveMergeSort);
     TestSort("Tests/big_tests", "TestsResults/Point3/IterativeMerge.out", 
                                             0, 1000000, 10000, 1, IterativeMergeSort);
     
-
     //-----------------------Point 4-------------------------------------------
 
-    
     TestSort("Tests/big_tests", "TestsResults/Point4/Lomuto_big_tests.out", 
                                             0, 1000000, 10000, 1, QsortLomuto);
     TestSort("Tests/big_tests", "TestsResults/Point4/Hoare_big_tests.out", 
@@ -47,8 +35,6 @@ int main()
 
     TestSort("Tests/big_tests", "TestsResults/Point4/Ternary_big_tests.out", 
                                             0, 1000000, 10000, 1, QsortTernary);
-
-    
 
     TestSort("Tests/test_most_duplicates", "TestsResults/Point4/Lomuto_duplicates_tests.out", 
                                             0, 1000000, 10000, 1, QsortLomuto);
@@ -58,15 +44,10 @@ int main()
     TestSort("Tests/test_most_duplicates", "TestsResults/Point4/Ternary_duplicates_tests.out", 
                                             0, 1000000, 10000, 1, QsortTernary);
 
-    
-
     TestSort("Tests/big_tests", "TestsResults/Point4/OneBranch_big_tests.out", 
                                             0, 1000000, 10000, 1, QsortOneBranch);
 
-    
-
     //-----------------------Point 5-------------------------------------------
-
     
     TestSort("Tests/big_tests", "TestsResults/Point5/HoareCenter.out", 
                                             0, 1000000, 10000, 1, QsortHoare);
@@ -78,58 +59,48 @@ int main()
     TestSort("Tests/big_tests", "TestsResults/Point5/HoareMedianRnd3.out", 
                                             0, 1000000, 10000, 1, QsortMedianRnd3Pivot); 
 
-    */
-
     //-----------------------Point 6-------------------------------------------
 
-    /*
     TestSort("Tests/very_small_tests", "TestsResults/Point6/HoareCenter.out", 
                                             1, 150, 1, 5, QsortHoare);
     TestSort("Tests/very_small_tests", "TestsResults/Point6/Shell.out", 
                                             1, 150, 1, 5, ShellSort);
-    */
 
     //-----------------------Point 7-------------------------------------------
 
-    //TestIntroSort("Tests/big_tests", "TestsResults/Point7/TestingDepth.out",
-    //                                        0, 1000000, 10000, 1, IntroSortTestConstant);
+    TestIntroSort("Tests/big_tests", "TestsResults/Point7/TestingDepth.out",
+                                            0, 1000000, 10000, 1, IntroSortTestConstant);
 
-    /*
     TestSort("Tests/big_tests", "TestsResults/Point7/IntroSort.out",
                                               0, 1000000, 10000, 1, IntroSort);
 
     TestSort("Tests/big_tests", "TestsResults/Point7/QuickSort.out",
                                             0, 1000000, 10000, 1, QsortHoare);
-    */
 
     //-----------------------Point 8-------------------------------------------
 
-    /*
     TestSort("Tests/big_tests", "TestsResults/Point8/LSD.out", 
                                             0, 1000000, 10000, 1, LSDSort);
     TestSort("Tests/big_tests", "TestsResults/Point8/MSD.out", 
                                             0, 1000000, 10000, 1, MSDSort);
     
-
    //-----------------------Point 9-------------------------------------------
 
-   TestSort("Tests/big_tests", "TestsResults/Point9/HeapSort.out",
+    TestSort("Tests/big_tests", "TestsResults/Point9/HeapSort.out",
                                             0, 1000000, 10000, 1, HeapSort);
 
-   TestSort("Tests/big_tests", "TestsResults/Point9/MergeIterative.out",
+    TestSort("Tests/big_tests", "TestsResults/Point9/MergeIterative.out",
                                             0, 1000000, 10000, 1, IterativeMergeSort);
 
-   TestSort("Tests/big_tests", "TestsResults/Point9/Hoare_Central.out",
+    TestSort("Tests/big_tests", "TestsResults/Point9/Hoare_Central.out",
                                             0, 1000000, 10000, 1, QsortHoare);
 
-   TestSort("Tests/big_tests", "TestsResults/Point9/Introsort.out",
+    TestSort("Tests/big_tests", "TestsResults/Point9/Introsort.out",
                                             0, 1000000, 10000, 1, IntroSort);
 
-   TestSort("Tests/big_tests", "TestsResults/Point9/LSD.out",
+    TestSort("Tests/big_tests", "TestsResults/Point9/LSD.out",
                                             0, 1000000, 10000, 1, LSDSort);
 
-   TestSort("Tests/big_tests", "TestsResults/Point9/QsortStd.out",
+    TestSort("Tests/big_tests", "TestsResults/Point9/QsortStd.out",
                                             0, 1000000, 10000, 1, QsortStd);
-
-    */
 }
