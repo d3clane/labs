7dc658b925b931c7804c166ba78963b19174b279
Arman <ovakimian.al@phystech.edu>
2024-04-25 13:37:30 +0300

    ops graphs

--- Lab5/ReadmeAssets/BuildHisto.py
+++ Lab5/ReadmeAssets/BuildHisto.py
@@ -1,19 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('TestsResults/StrHash/Sum.out')
+data = np.loadtxt('TestsResults/IntHash/Knuth.out')
 
 values_col1 = data[:, 0]
 heights_col2 = data[:, 1]
 
-values_col1_clipped = np.clip(values_col1, 0, 5000)
+#values_col1_clipped = np.clip(values_col1, 0, 5000)
 
-plt.bar(values_col1_clipped, heights_col2, color='skyblue', edgecolor='black')
+plt.bar(values_col1, heights_col2, color='skyblue', edgecolor='black')
 plt.xlabel('list id')
 plt.ylabel('list size')
 plt.title('histogram')
 plt.grid(True)
 
-#plt.ylim(top = 25)
+plt.ylim(top = 25)
 
-plt.savefig('ReadmeAssets/imgs/SumClose.png')
+plt.savefig('ReadmeAssets/imgs/Knuth.png')
